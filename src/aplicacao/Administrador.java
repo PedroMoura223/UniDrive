86ec143831954e9169e92317fbb7a7e098305c33
PedroMoura223 <pedromouras223@gmail.com>
2023-11-28 20:41:31 -0300

    Alteracoes Classes

--- src/aplicacao/Administrador.java
+++ src/aplicacao/Administrador.java
@@ -1,17 +1,28 @@
 package aplicacao;
+
 public class Administrador extends Usuario {
 
-  public int credencial;
+    private int credencial;
 
-    public EstacaoTicket trabalha;
+    public Administrador() {
 
-  public void cadastrarVeiculo() {
-  }
+        this.credencial = 0;
+    }
 
-  public void removerVeiculo() {
-  }
+    public int getCredencial() {
+        return credencial;
+    }
 
-  public void emitirRelatorio() {
-  }
+    public void setCredencial(int credencial) {
+        this.credencial = credencial;
+    }
 
+    public void cadastrarVeiculo() {
+    }
+
+    public void removerVeiculo() {
+    }
+
+    public void emitirRelatorio() {
+    }
 }
