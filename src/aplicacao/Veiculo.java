0a7e0b2384787ae7d2f8d4b505e70a316294ebae
GustavoCassimiro9 <gustavocassimiro60@gmail.com>
2023-11-28 23:18:35 -0300

    Criação do controller de veiculos

--- src/aplicacao/Veiculo.java
+++ src/aplicacao/Veiculo.java
@@ -1,19 +1,51 @@
 package aplicacao;
 
-public class Veiculo {
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
-  public int id;
+public class Veiculo implements Serializable{
+    private int id;
+    private String tipo;
+    private String estacao;
 
-  protected Boolean bateriaRemovivel;
+    public Veiculo(int id, String tipo, String estacao) {
+        this.id = id;
+        this.tipo = tipo;
+        this.estacao = estacao;
+    }
 
-  public String estacao;
+    public int getId() {
+        return id;
+    }
 
-  public void redefinirEstaticas() {}
+    public void setId(int id) {
+        this.id = id;
+    }
 
-  public void acelerar() {}
+    public String getTipo() {
+        return tipo;
+    }
 
-  public void desacelerar() {}
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 
-  public void freiar() {}
+    public String getEstacao() {
+        return estacao;
+    }
 
+    public void setEstacao(String estacao) {
+        this.estacao = estacao;
+    }
+       
+    @Override
+    public String toString() {
+        return "Veiculo [id=" + id + ", tipo=" + tipo + ", estacao=" + estacao + "]";
+    }
 }
