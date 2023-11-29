2daf8d6e81b057ccc91393bb32df89043854e1eb
PedroMoura223 <pedromouras223@gmail.com>
2023-11-28 22:12:34 -0300

    Implementação Main

--- src/aplicacao/Cliente.java
+++ src/aplicacao/Cliente.java
@@ -2,31 +2,50 @@ package aplicacao;
 
 public class Cliente extends Usuario {
 
-    private double saldo;
-    private int idVeiculo;
+    private float saldo;
 
-    public Cliente() {
-        this.saldo = 0.0;
-        this.idVeiculo = 0;
-    }
-
-    public double getSaldo() {
-        return saldo;
-    }
-
-    public void setSaldo(float saldo) {
+    public Cliente(int id, String nome, String email, float saldo) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
         this.saldo = saldo;
     }
 
-    public int getIdVeiculo() {
-        return idVeiculo;
+    public float getSaldo() {
+        return saldo;
+    }
+    
+    public void setSaldo(float saldo) {
+        this.saldo = saldo;
+    }
+    
+    public String getNome() {
+        return nome;
+    }
+    
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
     }
 
-    public void setIdVeiculo(int idVeiculo) {
-        this.idVeiculo = idVeiculo;
-    }
 
     public void alugarVeiculo() {
+       
     }
 
     public void devolverVeiculo() {
