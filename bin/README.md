6fc5bb86eba0695e13925e8e6b9aa9e9fde05906
PedroMoura223 <pedromouras223@gmail.com>
2023-11-29 18:41:14 -0300

    Commit Inicial

--- bin/README.md
+++
@@ -1,2 +0,0 @@
-# UniDrive
-Trabalho POO
--- src/aplicacao/Veiculo.java
+++ src/aplicacao/Veiculo.java
@@ -1,51 +1,19 @@
 package aplicacao;
 
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+public class Veiculo {
 
-public class Veiculo implements Serializable{
-    private int id;
-    private String tipo;
-    private String estacao;
+  public int id;
 
-    public Veiculo(int id, String tipo, String estacao) {
-        this.id = id;
-        this.tipo = tipo;
-        this.estacao = estacao;
-    }
+  protected Boolean bateriaRemovivel;
 
-    public int getId() {
-        return id;
-    }
+  public String estacao;
 
-    public void setId(int id) {
-        this.id = id;
-    }
+  public void redefinirEstaticas() {}
 
-    public String getTipo() {
-        return tipo;
-    }
+  public void acelerar() {}
 
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
+  public void desacelerar() {}
 
-    public String getEstacao() {
-        return estacao;
-    }
+  public void freiar() {}
 
-    public void setEstacao(String estacao) {
-        this.estacao = estacao;
-    }
-       
-    @Override
-    public String toString() {
-        return "Veiculo [id=" + id + ", tipo=" + tipo + ", estacao=" + estacao + "]";
-    }
 }
